cd76dabd5bb0cd9269b2aa0c6a7feaf3e07e1d4b
daekyeong <daekyeongp96@gmail.com>
2021-10-06 19:33:11 +0900

    performance improve

---
+++ src/components/TodoInsert.js
@@ -0,0 +1,34 @@
+import React, { useState, useCallback } from "react";
+import { MdAdd } from 'react-icons/md';
+import './TodoInsert.scss';
+
+const TodoInsert = ({onInsert}) => {
+    const [value, setValue] = useState('');
+
+    const onChange = useCallback(e => {
+        setValue(e.target.value);
+    }, []);
+
+    const onSubmit = useCallback(e => {
+        onInsert(value);
+        setValue(''); // value 값 초기화
+
+        // submit 이벤트가 브라우저를 새로고침하는 것을 방지
+        e.preventDefault();
+    }, [onInsert, value]);
+
+    return (
+        <form className="TodoInsert" onSubmit={onSubmit}>
+            <input 
+            placeholder="할 일을 입력하세요" 
+            value={value}
+            onChange={onChange}
+            />
+            <button type="submit">
+                <MdAdd />
+            </button>
+        </form>
+    );
+};
+
+export default TodoInsert;
